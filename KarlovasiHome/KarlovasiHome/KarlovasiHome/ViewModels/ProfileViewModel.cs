eaa86904c79ecb0427e3c3b6c495f1cab639b512
JohnStavrou <johnstavrou@hotmail.com>
2019-01-30 20:20:59 +0200

    profile page ready/signuppage fixes

---
+++ KarlovasiHome/KarlovasiHome/KarlovasiHome/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace KarlovasiHome.ViewModels
+{
+    public class ProfileViewModel : BaseViewModel
+    {
+        private bool _enabled;
+
+        public ProfileViewModel()
+        {
+            Loading = false;
+            Enabled = false;
+        }
+
+        public bool Enabled
+        {
+            get { return _enabled; }
+            set
+            {
+                _enabled = value;
+                OnPropertyChanged(nameof(Enabled));
+            }
+        }
+    }
+}
--- KarlovasiHome/KarlovasiHome/KarlovasiHome/Views/ProfilePage.xaml.cs
+++ KarlovasiHome/KarlovasiHome/KarlovasiHome/Views/ProfilePage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using KarlovasiHome.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -7,12 +8,21 @@ namespace KarlovasiHome.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ProfilePage : ContentPage
     {
+        private readonly ProfileViewModel _pvm;
+
         public ProfilePage()
         {
             InitializeComponent();
+
+            _pvm = (ProfileViewModel) BindingContext;
         }
 
         private void Settings_OnClicked(object sender, EventArgs e)
+        {
+            _pvm.Enabled = !_pvm.Enabled;
+        }
+
+        private void ProfileImage_OnTapped(object sender, EventArgs e)
         {
             
         }
