61fc20300573bd654230b9e63c50260153a7c4cb
JohnStavrou <johnstavrou@hotmail.com>
2019-02-05 05:01:48 +0200

    splash screen fix

--- KarlovasiHome/KarlovasiHome/KarlovasiHome.Android/MainActivity.cs
+++ KarlovasiHome/KarlovasiHome/KarlovasiHome.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Plugin.CurrentActivity;
 
 namespace KarlovasiHome.Droid
 {
-    [Activity(Label = "Karlovasi Home", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
---
+++ KarlovasiHome/KarlovasiHome/KarlovasiHome.Android/SplashActivity .cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.Support.V7.App;
+
+namespace KarlovasiHome.Droid
+{
+    [Activity(Label = "Karlovasi Home", Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        void SimulateStartup()
+        {
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
